e6c1b0e15a32020a1564dd38e2d0214dd6537266
FarhanUddin <nasdillabeat@gmail.com>
2018-12-23 17:05:03 -0500

    db connekted

---
+++ src/databases/Test.java
@@ -0,0 +1,12 @@
+package databases;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class Test {
+    public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
+        ConnectToSqlDB.connectToSqlDatabase();
+    }
+
+
+}
